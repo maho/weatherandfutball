0e872bf42897a4456495c8db1ac448f319430046
maho <maho@pagema.net>
2018-08-17 15:16:52 +0200

    - forgot about __main__ section in script

--- football.py
+++ football.py
@@ -4,3 +4,9 @@ def min_for_against():
     return min_identifier("http://codekata.com/data/04/football.dat", 
                           (7,23), (43,47), (50,56), ignrows=1, ignids='-'*17)
 
+
+if __name__ == '__main__':  # pragma: no cover
+    print('Team with min for-against difference is  :', min_for_against())
+
+
+
